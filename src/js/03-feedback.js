a1df9e8607d04a3275a1bce88eb34d35f615b3ed
volodymyr1v <volodumur.verhyn@gmail.com>
2023-05-28 16:38:22 +0300

    update form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,38 +1,36 @@
 import throttle from "lodash.throttle";
 
-const STORAGE_KEY = 'feedback-form-state';
-
 const formEl = document.querySelector('.feedback-form');
-const emailEl = document.querySelector('.feedback-form input');
-const textareaEl = document.querySelector('.feedback-form textarea');
 
-formEl.addEventListener('input', throttle(onFormInput, 500));
-formEl.addEventListener('submit', onFormSubmit);
-document.addEventListener('DOMContentLoaded', onSiteReload);
+const STORAGE_KEY = "feedback-form-state";
+let formData = {};
 
-function onFormInput() {
-    const data = {};
-    data.email = emailEl.value;
-    data.message = textareaEl.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
-    console.log(data)
+formEl.addEventListener('input', throttle(formInput, 500));
+formEl.addEventListener('submit', formSubmit);
+window.addEventListener('load', onLoadForm);
+
+
+function formInput (event) {
+    const { name, value } = event.target;
+    formData[name] = value.trim();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 };
 
-let retDataItem = localStorage.getItem(STORAGE_KEY);
-
-function onSiteReload() {
-    if (retDataItem) {
-        let storageData = JSON.parse(retDataItem);
-        emailEl.value = storageData.email;
-        textareaEl.value = storageData.message;
-    }
-}
-
-function onFormSubmit(event) {
+function formSubmit (event) {
     event.preventDefault();
-    console.log(JSON.parse(retDataItem));
     localStorage.removeItem(STORAGE_KEY);
-    emailEl.value = '';
-    textareaEl.value = '';
-}
+    event.target.reset();
+};
 
+function onLoadForm () {
+   try {
+    const saveData = localStorage.getItem(STORAGE_KEY);
+    if (!saveData) return;
+    formData = JSON.parse(saveData);
+    Object.entries(formData).forEach(([key, val]) => {
+        formEl.elements[key].value = val;
+    })
+   } catch (error) {
+    console.log(error.message);
+   }
+};
